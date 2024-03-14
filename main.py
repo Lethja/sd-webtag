a90523d352d27d8344395e4d32121b97a10cdf26
Jason Lethbridge <lethbridgejason@gmail.com>
2024-03-15 03:27:35 +1100

    Added callbacks to load pictures of a set into the gallery when selected

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import gradio as gr
 
 from utility import *
 
+startup_tag = startup_check()
+
 with gr.Blocks() as SDWebTag:
     with gr.Row():
         with gr.Column():
             tag_set = gr.Dropdown(label="Tag Set", allow_custom_value=True, elem_id="tag_set",
-                                  choices=generate_tag_set_list())
+                                  choices=generate_tag_set_list(), value=startup_tag)
             gallery = gr.Gallery(label="Gallery", show_label=False, elem_id="gallery", object_fit="contain",
-                                 type="filepath")
+                                 type="filepath", value=populate_gallery(startup_tag))
         with gr.Column():
             add = gr.Textbox(label="Add Tags")
             tags = gr.CheckboxGroup(label="Tags", elem_id="tags")
+
+    tag_set.input(switch_tag_set, inputs=tag_set, outputs=gallery)
 
 SDWebTag.launch()
--- utility.py
+++ utility.py
@@ -12,14 +12,41 @@ def generate_tag_set_list():
         if os.path.isdir(os.path.join(tag_set_dir, e)):
             r.append(e)
 
+    r.sort()
     return r
 
 
-def populate_gallery():  # TODO: Call when tag set changes and get all images in the new location
+def populate_gallery(tag_set) -> list[str]:  # TODO: Call when tag set changes and get all images in the new location
     r = []
     ex = ("*.jpeg", "*.jpg", "*.png", "*.svg")
+    d = os.path.join(tag_set_dir, tag_set)
 
     for e in ex:
-        r.extend(glob.glob(os.path.join(tag_set_dir, e)))
+        r.extend(glob.glob(os.path.join(d, e)))
 
-    return r.sort()
+    return r
+
+
+def startup_check():
+    if not os.path.isdir(tag_set_dir):
+        os.mkdir(tag_set_dir)
+
+    existing_tag_sets = generate_tag_set_list()
+
+    if len(existing_tag_sets) == 0:
+        set_tag = "Default"
+        os.mkdir(os.path.join(tag_set_dir, set_tag))
+    else:
+        set_tag = existing_tag_sets[0]
+
+    return set_tag
+
+
+def switch_tag_set(tag_set):
+    if tag_set is not list:
+        fullpath = os.path.join(tag_set_dir, tag_set)
+        if not os.path.isdir(fullpath):
+            os.mkdir(fullpath)
+
+    return populate_gallery(tag_set)
+
