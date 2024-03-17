479a83dc6f3fdbdf42b5a35eba61893f0b729a5f
Jason Lethbridge <lethbridgejason@gmail.com>
2024-03-18 02:06:25 +1100

    Added accordions to hide low use part of the interface

--- event.py
+++ event.py
@@ -9,5 +9,8 @@ def callback_update_gallery(tag_set):
 
 def callback_update_tag_dropdown(choice):
     tag_set_directory(choice)
-    return gr.Dropdown(label="Tag Set", allow_custom_value=True, elem_id="tag_set", choices=generate_tag_set_list(),
+    return gr.Dropdown(show_label=False,
+                       allow_custom_value=True,
+                       elem_id="tag_set",
+                       choices=generate_tag_set_list(),
                        value=choice)
--- main.py
+++ main.py
@@ -8,11 +8,23 @@ def init():
     with gr.Blocks() as SDWebTag:
         with gr.Row():
             with gr.Column():
-                tag_set_dropdown = callback_update_tag_dropdown(startup_tag)
-                gallery = gr.Gallery(label="Gallery", show_label=False, elem_id="gallery", object_fit="contain",
-                                     type="filepath", value=populate_gallery(startup_tag))
+                with gr.Accordion("Tag Set"):
+                    tag_set_dropdown = callback_update_tag_dropdown(startup_tag)
+                    with gr.Blocks():
+                        with gr.Row():
+                            upload = gr.UploadButton("Import")
+                            download = gr.DownloadButton("Export")
+
+                gallery = gr.Gallery(label="Gallery",
+                                     show_label=False,
+                                     elem_id="gallery",
+                                     object_fit="contain",
+                                     type="filepath",
+                                     value=populate_gallery(startup_tag))
             with gr.Column():
-                add = gr.Textbox(label="Add Tags")
+                with gr.Accordion("Add Tags", open=False):
+                    add = gr.Textbox(label="Add Tags", show_label=False)
+
                 tags = gr.CheckboxGroup(label="Tags", elem_id="tags")
 
         tag_set_dropdown.input(callback_update_tag_dropdown, inputs=tag_set_dropdown, outputs=tag_set_dropdown)
