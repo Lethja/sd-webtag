cb4f918bd4b6612786c3367bb0d0a2e18570b118
Jason Lethbridge <lethbridgejason@gmail.com>
2024-03-18 15:46:01 +1100

    Remember the last tag set selected between reloads

--- event.py
+++ event.py
@@ -2,10 +2,19 @@ import gradio as gr
 
 from utility import *
 
+global_tag_set_state = None
+
 
 def event_download_tag_set():
     # Placeholder
     print("Download Pressed")
+
+
+def event_load_page():
+    global global_tag_set_state
+    d = event_update_tag_dropdown(global_tag_set_state)
+    g = populate_gallery(global_tag_set_state)
+    return d, g
 
 
 def event_upload_files(files, tag_set):
@@ -19,8 +28,14 @@ def event_update_gallery(tag_set):
 
 def event_update_tag_dropdown(choice):
     tag_set_directory(choice)
+    global global_tag_set_state
+    global_tag_set_state = choice
     return gr.Dropdown(show_label=False,
                        allow_custom_value=True,
                        elem_id="tag_set",
                        choices=generate_tag_set_list(),
                        value=choice)
+
+
+def event_update_tag_state(choice):
+    return choice
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from utility import *
 
 
 def init():
-    startup_tag = startup_check()
+    global global_tag_set_state
+    global_tag_set_state = startup_check()
 
     with gr.Blocks() as SDWebTag:
         with gr.Row():
             with gr.Column():
                 with gr.Accordion("Tag Set"):
-                    tag_set_dropdown = event_update_tag_dropdown(startup_tag)
+                    tag_set_dropdown = event_update_tag_dropdown(global_tag_set_state)
                     with gr.Blocks():
                         with gr.Row():
                             upload = gr.UploadButton(label="Import",
@@ -21,18 +22,27 @@ def init():
                                      show_label=False,
                                      elem_id="gallery",
                                      object_fit="contain",
-                                     type="filepath",
-                                     value=populate_gallery(startup_tag))
+                                     type="filepath")
             with gr.Column():
                 with gr.Accordion("Add Tags", open=False):
                     add = gr.Textbox(label="Add Tags", show_label=False)
 
                 tags = gr.CheckboxGroup(label="Tags", elem_id="tags")
 
-        SDWebTag.load(event_update_gallery, inputs=tag_set_dropdown, outputs=gallery)
-        tag_set_dropdown.input(event_update_tag_dropdown, inputs=tag_set_dropdown, outputs=tag_set_dropdown)
-        tag_set_dropdown.input(event_update_gallery, inputs=tag_set_dropdown, outputs=gallery)
-        upload.upload(event_upload_files, inputs=[upload, tag_set_dropdown], outputs=gallery)
+        SDWebTag.load(event_load_page, outputs=[tag_set_dropdown, gallery])
+
+        tag_set_dropdown.input(event_update_tag_dropdown,
+                               inputs=tag_set_dropdown,
+                               outputs=tag_set_dropdown)
+
+        tag_set_dropdown.input(event_update_gallery,
+                               inputs=tag_set_dropdown,
+                               outputs=gallery)
+
+        upload.upload(event_upload_files,
+                      inputs=[upload, tag_set_dropdown],
+                      outputs=gallery)
+
         download.click(event_download_tag_set)
 
     return SDWebTag
